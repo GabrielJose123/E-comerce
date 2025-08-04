28e686bb52ca572f3fd0d8efda7f691091fe0025
GabrielJose <gabrieljoseroberto@hotmail.com>
2025-08-03 21:22:35 -0300

    criando mais uma parte do header

--- src/components/SecHeader.jsx
+++ src/components/SecHeader.jsx
@@ -1,4 +1,5 @@
 import logo from '../assets/logo.png.svg'
+import Lupa from '../assets/lupa.png'
 
 export const SecHeader = () => {
     return (
@@ -6,7 +7,15 @@ export const SecHeader = () => {
             <div style={{height:'100%',width:'15%',display:'flex',alignItems:'center',justifyContent:'center',backgroundColor:'white'}}>
                 <img src={logo} alt="" />
             </div>
-            
+
+            <div className='HeadInput'>
+                <div className='inputChild'>
+                    <input className='SearchHead' placeholder='Pesquise Algo' type="text" />
+                    <img style={{cursor:'pointer', marginLeft:'3rem'}} src={Lupa} alt="" />
+                </div>
+            </div>
+
+            <div></div>
         </div>
     );
 };
